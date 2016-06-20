457f7691d25a873cf31824962707d7ab912f14a9
raynamharris <rayna.harris@utexas.edu>
2016-06-19 22:29:26 -0400

    script updates

--- bin/NSB_impact.R
+++ bin/NSB_impact.R
@@ -88,23 +88,94 @@ plot(impact_g)
 ###################################
 ## now for importance
 ## read in and clean data
+mydata <- "NSB_impact.csv"
 mydata <- read.csv(mydata, header=TRUE, na.strings=c("NA","NaN", " ", "") )
-names(mydata)[35] <- "Lectures"
-names(mydata)[36] <- "HandsOnExercises"
-names(mydata)[37] <- "IndependentProjects"
-names(mydata)[38] <- "StudentPresentations"
-names(mydata)[39] <- "InterdisciplinaryResearchExperience"
-names(mydata)[40] <- "ProfessionalDevelopment"
+names(mydata)[45] <- "Lectures"
+names(mydata)[46] <- "HandsOnExercises"
+names(mydata)[47] <- "IndependentProjects"
+names(mydata)[48] <- "StudentPresentations"
+names(mydata)[49] <- "InterdisciplinaryResearchExperience"
+names(mydata)[50] <- "LoanerEquipment"
+names(mydata)[51] <- "ProfessionalDevelopment"
+names(mydata)[13] <- "role"
+
+mydata <- mydata %>% 
+  filter(role %in% c("Course Director", "Course Assistant", "Faculty", "Student", "Teaching Assistant")) %>%
+  filter(Lectures != "NA") %>%
+  filter(HandsOnExercises != "NA") %>%
+  filter(IndependentProjects != "NA") %>%
+  filter(StudentPresentations != "NA") %>%
+  filter(InterdisciplinaryResearchExperience != "NA") %>%
+  filter(LoanerEquipment != "NA") %>%
+  filter(ProfessionalDevelopment != "NA")
 
 ## subset and order data
 importance <- mydata %>%
   select(Lectures, HandsOnExercises, IndependentProjects, StudentPresentations,
-         InterdisciplinaryResearchExperience, ProfessionalDevelopment) %>%
-  rename(c(HandsOnExercises = "Hands-on Exercises", 
-           IndependentProjects = "Independent Projects", 
-           StudentPresentations = "Student Presentations",
-           InterdisciplinaryResearchExperience = "Interdisciplinary Research Experience",
-           ProfessionalDevelopment = "Professional Development))
-summary(impact)
-str(impact)
+         InterdisciplinaryResearchExperience, LoanerEquipment, ProfessionalDevelopment) #%>%
+  #rename(c(HandsOnExercises = "Hands-on Exercises", 
+           #IndependentProjects = "Independent Projects", 
+           #StudentPresentations = "Student Presentations",
+           #InterdisciplinaryResearchExperience = "Interdisciplinary Research Experience",
+           #LoanerEquipment = "Loaner Equipment",
+           #ProfessionalDevelopment = "Professional Development"))
+summary(importance)
+str(importance)
 
+importance$"HandsOnExercises" <- factor(importance$"HandsOnExercises", 
+                                   c(#"Not applicable",
+                                     "Very unimportant", 
+                                     "Unimportant", 
+                                     "Neither important or unimportant",
+                                     "Important",
+                                     "Very Important"))
+importance$"Lectures" <- factor(importance$"Lectures", 
+                                          c(#"Not applicable",
+                                            "Very unimportant", 
+                                            "Unimportant", 
+                                            "Neither important or unimportant",
+                                            "Important",
+                                            "Very Important"))
+
+importance$"StudentPresentations" <- factor(importance$"StudentPresentations", 
+                                c(#"Not applicable",
+                                  "Very unimportant", 
+                                  "Unimportant", 
+                                  "Neither important or unimportant",
+                                  "Important",
+                                  "Very Important"))
+
+importance$"InterdisciplinaryResearchExperience" <- factor(importance$"InterdisciplinaryResearchExperience", 
+                                            c(#"Not applicable",
+                                              "Very unimportant", 
+                                              "Unimportant", 
+                                              "Neither important or unimportant",
+                                              "Important",
+                                              "Very Important"))
+
+importance$"ProfessionalDevelopment" <- factor(importance$"ProfessionalDevelopment", 
+                                                           c(#"Not applicable",
+                                                             "Very unimportant", 
+                                                             "Unimportant", 
+                                                             "Neither important or unimportant",
+                                                             "Important",
+                                                             "Very Important"))
+
+importance$"LoanerEquipment" <- factor(importance$"LoanerEquipment", 
+                                                           c(#"Not applicable",
+                                                             "Very unimportant", 
+                                                             "Unimportant", 
+                                                             "Neither important or unimportant",
+                                                             "Important",
+                                                             "Very Important"))
+
+importance$"IndependentProjects" <- factor(importance$"IndependentProjects", 
+                                       c(#"Not applicable",
+                                         "Very unimportant", 
+                                         "Unimportant", 
+                                         "Neither important or unimportant",
+                                         "Important",
+                                         "Very Important"))
+
+importance_g <- likert(importance, grouping = mydata$role)
+plot(importance_g)
